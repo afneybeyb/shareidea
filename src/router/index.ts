0a05297b3638a0e5d69c109d7da69ecba09e5c29
afneybeyb <afneybeyb@gmail.com>
2022-07-29 11:46:04 +0200

    new route - id parameter shows idea with that id

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import IdeaView from "../views/IdeaView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: HomeView,
+		},
+		{
+			path: "/:id",
+			name: "idea",
+			component: IdeaView,
+			props: true,
+		},
+	],
 });
 
 export default router;
