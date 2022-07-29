b1af9031304015cd601487dfde59dea996b9021c
afneybeyb <afneybeyb@gmail.com>
2022-07-29 11:43:49 +0200

    shows idea if it exists

---
+++ src/components/ShowIdea.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { getIdea } from "../firebase";
+
+const props = defineProps({
+	id: {
+		type: String,
+		default: "",
+		required: true,
+	}
+});
+
+let idea = await getIdea(props.id);
+
+// Shows either the idea or a sorry message
+let note = idea ? idea.note : "Failed to find your idea. 🥺 Check your link and try again.";
+</script>
+
+<template>
+	<p>
+		{{ note }}
+	</p>
+</template>
+
+<style scoped>
+</style>
+
